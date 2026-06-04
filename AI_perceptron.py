9df631dcce611753cfe8c882fed95e25113fce1c
Syed Salman Sami <syedsamigithub@gmail.com>
2026-06-04 19:36:41 +0600

    The Perceptron defines the first step into Neural Networks.

---
+++ AI_perceptron.py
@@ -0,0 +1,15 @@
+
+# Python Perceptron
+
+print("Will I go to the concert?")
+
+threshold = 1.5
+inputs = [1, 0, 1, 0, 1]
+weights = [0.7, 0.6, 0.5, 0.3, 0.4]
+
+sum_value = 0
+
+for i in range(len(inputs)):
+    sum_value += inputs[i] * weights[i]
+
+print(sum_value > threshold)
